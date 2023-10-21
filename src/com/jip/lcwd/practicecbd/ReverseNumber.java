7d8ff5356e73c448d1a2102826a3331caab00052
Shankarlal Sharma <shankarjsharma@gmail.com>
2023-10-22 00:16:33 +0530

    Updated ReverseNumber.java
    ReverseNumber.java needs to be completed

--- src/com/jip/lcwd/practicecbd/ReverseNumber.java
+++ src/com/jip/lcwd/practicecbd/ReverseNumber.java
@@ -4,6 +4,18 @@ public class ReverseNumber {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int num = 57359;
+		int reverse;
+		int r;
+		
+		while(num>0) {
+			r = num%10;
+//			reverse = reverse + r;
+			
+		}
+		// Complete it!!!
+		
 
 	}
 
